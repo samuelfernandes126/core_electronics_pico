eb3452e5f1fd89f8e9d34a6b83afe67594c9b83b
Samuel Fernandes <98076575+samuelfernandes126@users.noreply.github.com>
2024-08-16 07:47:05 +0530

    added extension
    added the .py extension to the file and also improved the comments

--- pico 1.py
+++ pico 1.py
@@ -1,7 +1,7 @@
 from machine import Pin
 import time
 
-led = Pin("LED", Pin.OUT) # On board LED = "LED", 
+led = Pin("LED", Pin.OUT) # 16 for GPIO 16, "LED" for using onboard LED
 
 while True:  #Infinite Loop
 	led.value(1) # Turn LED on
